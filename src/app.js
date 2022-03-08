e11db648dc58d3ee04a3df2e06d7ea00410d11ac
sangluong2k2 <luongduysang2k2@gmail.com>
2022-03-08 12:56:33 +0700

    feat: app.js  index.html

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 20;
+function sum(numA, numB) {
+    return numA + numB;
+}
+sum(a, b);
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+const a: number = 10;
+const b: number = 20;
+
+function sum(numA: number, numB: number): number {
+    return numA + numB;
+}
+
+sum(a,b);
+
